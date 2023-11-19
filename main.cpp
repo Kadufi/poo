78c45d0a24c17e56920ea7a6f483d122624a0603
Kadu <carlosperinifidelis@gmail.com>
2023-11-19 17:42:25 -0300

    poteiro

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ int main(){
     std::cout << "volume: "<< cilindro1.volumeDoCilindro() <<std::endl;
     std::cout << "h:" << cilindro1.hDoCilindro() << std::endl;
     std::cout << "r:" << cilindro1.rDoCilindro() << std::endl;
-    cilindro1.dobra_H_DoCilindro(cilindro1.hDoCilindro());
-    std::cout << "volume: "<< cilindro1.volumeDoCilindro() <<std::endl;
-    std::cout << "h:" << cilindro1.hDoCilindro() << std::endl;
-    std::cout << "r:" << cilindro1.rDoCilindro() << std::endl;
+    Cilindro *pc1 = &cilindro1;
+    std::cout << "volume: "<< pc1->volumeDoCilindro() <<std::endl;
+    std::cout << "h:" << pc1->hDoCilindro() << std::endl;
+    std::cout << "r:" << pc1->rDoCilindro() << std::endl;
 
     return 0;
 }
